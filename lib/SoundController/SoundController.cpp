62e0e9ee9616051d96da17bb84ebb57b9c838cbc
Pawel A. <pawel.adamski25@wp.pl>
2025-09-05 19:50:34 +0200

    Refactor: Rework SoundController to use custom I2S driver
    Removed AudioOutputI2S and AudioOutputMixer due to unsatisfactory playback performance
    (gaps between sounds, poor mixing, limited control).
    Now using "driver/i2s.h" with a custom audio library for managing sounds.
    Inspired by WAV mixing example from http://www.xtronical.com/i2s-ep4.
    Current audio files are in a test state.

--- lib/SoundController/SoundController.cpp
+++ lib/SoundController/SoundController.cpp
@@ -4,12 +4,14 @@
 SoundController::SoundController(int bclkPin, int lrclkPin, int dinPin,
                                  int _sd_sckPin, int _sd_misoPin, int _sd_mosiPin, int _sd_csPin)
     : _bclkPin(bclkPin), _lrclkPin(lrclkPin), _dinPin(dinPin),
-      _engineOn(false), _blinkerOn(false), _hornOn(false), _engineRpm(0)
+      _engineOn(false), _blinkerOn(false), _hornOn(false), _engineRpm(0),
+      _sd_sckPin(_sd_sckPin), _sd_misoPin(_sd_misoPin), _sd_mosiPin(_sd_mosiPin), _sd_csPin(_sd_csPin)
 {
 }
 
 void SoundController::begin()
 {
+
     SPI.begin(_sd_sckPin, _sd_misoPin, _sd_mosiPin, _sd_csPin);
     if (!SD.begin(_sd_csPin, SPI))
     {
@@ -19,19 +21,52 @@ void SoundController::begin()
     }
     Serial.println("SD Card OK");
 
-    outI2S = new AudioOutputI2S();
-    outI2S->SetPinout(_bclkPin, _lrclkPin, _dinPin);
-    outI2S->begin();
+    i2s_pin_config_t pin_config =
+        {
+            .bck_io_num = _bclkPin,          // The bit clock connectiom, goes to pin 27 of ESP32
+            .ws_io_num = _lrclkPin,          // Word select, also known as word select or left right clock
+            .data_out_num = _dinPin,         // Data out from the ESP32, connect to DIN on 38357A
+            .data_in_num = I2S_PIN_NO_CHANGE // we are not interested in I2S data into the ESP32
+        };
 
-    mixer = new AudioOutputMixer(16, outI2S);
-    engineStartItem = new SampleItem("engineStart", mixer, "/engine_start1.mp3", 1.0f, false);
-    engineRuningItem = new SampleItem("endgineRun", mixer, "/engine_runing1.mp3", 0.5f, true);
-    hornItem = new SampleItem("horn", mixer, "/horn1.mp3", 1.0f, false);
-    blinkerItem = new SampleItem("blinker", mixer, "/blinker.mp3", 1.0f, true);
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM_0, &pin_config);
 
-    xTaskCreatePinnedToCore(_soundControllerTask, "soundLoopTask", 4096, this, 1, NULL, 1);
-    xTaskCreatePinnedToCore(_loopTask, "soundLoopTask", 4096, this, 1, NULL, 1);
-     Serial.println("");
+    Serial.println("");
+    musicItem = new AudioClip("/music1.wav", 0.2f, false);
+   // blinkerStartItem = new AudioClip("/blinker_start.wav", 1.0f, true);
+    blinkerRunItem   = new AudioClip("/blinker_runing.wav", 1.0f, true);
+   // blinkerEndItem   = new AudioClip("/blinker_end.wav", 1.0f, true);
+    
+    
+    int choice=random(0, 2);
+    const String horn_filename[2]= {"/horn1.wav", "/horn2.wav"};
+    hornItem =  new AudioClip(horn_filename[choice], 1.0f, false); 
+                            
+    
+    engineStartItem = new AudioClip("/engine_start1.wav", 0.2f, false);
+    engineRuningItem = new AudioClip("/engine_runing1.wav", 0.2f, true);
+    //engineStopItem = new AudioClip("/engine_stop1.wav", 0.2f, true);
+
+    
+
+    _clipList.push_back(musicItem);
+    _clipList.push_back(engineRuningItem);
+    _clipList.push_back(engineStartItem);    
+    //_clipList.push_back(engineStopItem);
+    
+   // _clipList.push_back(blinkerStartItem);
+    _clipList.push_back(blinkerRunItem);
+   // _clipList.push_back(blinkerEndItem);
+    _clipList.push_back(hornItem);
+
+    vTaskDelay(100);
+
+    Serial.println("");
+    xTaskCreatePinnedToCore(_soundControllerTask, "_soundControllerTask", 4096, this, 1, NULL, 1);
+    xTaskCreatePinnedToCore(_loopTask, "_soundControllerTask", 4096, this, 1, NULL, 1);
+
+    Serial.println("");
     Serial.println("SoundController::begin complette ");
 }
 
@@ -41,121 +76,202 @@ void SoundController::_soundControllerTask(void *param)
     bool isEnginStarted = false;
     bool isAddedQueueFlag = false;
     auto *parent = static_cast<SoundController *>(param);
-    auto *engineStartItem = parent->engineStartItem;
-    auto *engineRuningItem = parent->engineRuningItem;
-    auto *hornItem = parent->hornItem;
-    auto *blinkerItem = parent->blinkerItem;
 
     bool engineOn = parent->_engineOn;
     bool blinkerOn = parent->_blinkerOn;
     int engineRpm = parent->_engineRpm;
     bool engineRunPrev = false;
+    bool engineStarted = false;
+    Serial.println("_soundControllerTask play.");
+    parent->musicItem->play();
+
+    int test_i = 10;
+    int test_y = 20;
     while (true)
-    {        
-        if (parent->_hornOn)
+    {
+        test_i--;
+        test_y--;
+
+        if (test_i == 0)
         {
-            hornItem->play();
-            vTaskDelay(1000);
-            parent->_hornOn = false;
+            Serial.println("");
+            Serial.println("");
+            Serial.println("_soundControllerTask blinker on!");
+            parent->blinkerRunItem->play();
+            test_i=100;
         }
 
-        if (parent->_blinkerOn && !blinkerOn)
+        if (test_y == 0)
         {
-            blinkerOn = true;
-            engineRunPrev = engineOn;            
-            blinkerItem->play();
+            Serial.println("");
+            Serial.println("");
+            Serial.println("_soundControllerTask horn_train on!");
+            parent->hornItem->play();
+            test_y=80;
         }
 
-        if (parent->_engineOn && !engineOn)
+        if (!parent->musicItem->isPlaying())
         {
-            engineOn = true;
-            if (!engineRuningItem->isPlaying())
-            {
-                blinkerItem->stop();
-                parent->_blinkerOn=false;
-                vTaskDelay(1000);
-                engineStartItem->play();
-                while (engineStartItem->isPlaying())
-                {
-                    vTaskDelay(1);
-                }
-                engineRuningItem->play();
-            }
+            Serial.println("");
+            Serial.println("");
+            Serial.println("_soundControllerTask wait 10s");
+            vTaskDelay(10000);
+            parent->musicItem->play();
         }
 
-        if (!parent->_engineOn && engineOn)
-        {
-            engineOn = false;
-            engineRunPrev = false;
-            engineRuningItem->stop();
-        }
+        /*if (parent->_hornOn)
+         {
+             parent->stopAll();
+             playNext = NULL;
+             if (parent->_blinkerOn)
+                 playNext = blinkerItem;
+             else if (parent->_engineOn)
+                 playNext = engineRuningItem;
 
-        if (!parent->_blinkerOn && blinkerOn)
-        {
-            blinkerOn = false;
-            if (engineRunPrev)
-                engineRuningItem->play();
-            blinkerItem->stop();
-        }
+             hornItem->play(playNext);
 
-        if (engineRpm != parent->_engineRpm)
-        {
-            engineRuningItem->setRate(parent->_engineRpm);
-            engineRpm = parent->_engineRpm;
-        }
-        vTaskDelay(100);
+             while (hornItem->isPlaying())
+                 vTaskDelay(10);
+             parent->_hornOn = false;
+         }
+
+         if (parent->_blinkerOn && !blinkerOn)
+         {
+             parent->stopAll();
+             Serial.println("SoundController::_soundControllerTask - START BLINKER");
+             blinkerOn = true;
+             engineRunPrev = engineOn;
+             blinkerItem->play();
+         }
+
+         if (parent->_engineOn && !engineOn)
+         {
+             Serial.println();
+             Serial.println("SoundController::_soundControllerTask - START ENGINE");
+             engineOn = true;
+             if (!engineRuningItem->isPlaying())
+             {
+                 parent->stopAll();
+                 parent->_blinkerOn = false;
+                 vTaskDelay(50);
+                 if (!engineStarted)
+                 {
+                     engineStarted = true;
+                     engineRuningItem->loadData();
+                     engineStartItem->play(engineRuningItem);
+                 }
+             }
+         }
+
+         if (!parent->_engineOn && engineOn)
+         {
+             engineOn = false;
+             engineRunPrev = false;
+             engineStarted = false;
+             engineRuningItem->stop();
+         }
+
+         if (!parent->_blinkerOn && blinkerOn)
+         {
+             blinkerOn = false;
+             blinkerItem->stop();
+             vTaskDelay(50);
+             if (engineRunPrev)
+                 engineRuningItem->play();
+         }
+
+         if (engineRpm != parent->_engineRpm)
+         {
+             engineRuningItem->setRate(parent->_engineRpm);
+             engineRpm = parent->_engineRpm;
+         }*/
+        vTaskDelay(1000);
     }
 }
 
 void SoundController::_loopTask(void *param)
 {
     auto *parent = static_cast<SoundController *>(param);
-    if(!parent)
-    {
-        Serial.println("Parent null!!!");    
-        while(1);
-    }
     Serial.println("_loopTask runing.");
+    vTaskDelay(5000);
     while (true)
-    {        
+    {
         parent->loop();
+        vTaskDelay(1);
     }
 }
 
-void SoundController::startEngine()
+void SoundController::startEngine(const char *who)
 {
+    if (!_engineOn)
+        serialPrint("startEngine", who);
     _engineOn = true;
 }
 
-void SoundController::stopEngine()
+void SoundController::stopEngine(const char *who)
 {
+    if (_engineOn)
+        serialPrint("stopEngine", who);
     _engineOn = false;
 }
 
-void SoundController::setEngineRpm(uint16_t rpm)
+void SoundController::setEngineRpm(const char *who, uint16_t rpm)
 {
+    if (_engineRpm != rpm)
+        serialPrint("setEngineRpm", who);
+
     _engineRpm = rpm;
 }
 
-void SoundController::startBlinker()
+void SoundController::startBlinker(const char *who)
 {
+    if (!_blinkerOn)
+        serialPrint("startBlinker", who);
+
     _blinkerOn = true;
 }
 
-void SoundController::stopBlinker()
+void SoundController::stopBlinker(const char *who)
 {
+    if (_blinkerOn)
+        serialPrint("stopBlinker", who);
+
     _blinkerOn = false;
 }
 
-void SoundController::playHorn()
+void SoundController::serialPrint(const char *procName, const char *who)
 {
+    Serial.println();
+    Serial.print(" >>> SoundController:");
+    Serial.print(procName);
+    Serial.print(" called from: ");
+    Serial.print(who);
+    Serial.println();
+}
+
+void SoundController::playHorn(const char *who)
+{
+    Serial.println("SoundController::playHorn()");
+    Serial.print(who);
     _hornOn = true;
 }
 
 void SoundController::loop()
-{   
-    engineStartItem->loop();   
-    engineRuningItem->loop();   
-    hornItem->loop();   
-    blinkerItem->loop();   
+{
+    /*engineStartItem->loop();
+    engineRuningItem->loop();
+    hornItem->loop();
+    blinkerItem->loop();*/
+    AudioClip::loop(_clipList);
+}
+
+void SoundController::stopAll()
+{
+    Serial.println("SoundController::stopAll()");
+    /*engineRuningItem->stop();
+    engineStartItem->stop();
+    hornItem->stop();
+    blinkerItem->stop();*/
+    musicItem->stop();
+    vTaskDelay(100);
 }
--- lib/SoundController/SoundController.h
+++ lib/SoundController/SoundController.h
@@ -2,9 +2,23 @@
 #define SOUND_CONTROLLER_H
 
 #include <Arduino.h>
-#include "SampleItem.h"
-#include "AudioOutputI2S.h"
-#include "AudioOutputMixer.h"
+#include "SD.h"
+#include "driver/i2s.h"
+#include "AudioClip.h"
+
+static const i2s_config_t i2s_config =
+    {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = 44100, // Note, all files must be this
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_I2S | I2S_COMM_FORMAT_STAND_MSB),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // high interrupt priority
+        .dma_buf_count = 8,                       // 8 buffers
+        .dma_buf_len = 256,                       // 256 bytes per buffer, so 2K of buffer space
+        .use_apll = 0,
+        .tx_desc_auto_clear = true,
+        .fixed_mclk = -1};
 
 class SoundController
 {
@@ -14,16 +28,18 @@ public:
 
     void begin();
 
-    void startEngine();
-    void stopEngine();
-    void setEngineRpm(uint16_t rpm);
+    void startEngine(const char *who);
+    void stopEngine(const char *who);
+    void setEngineRpm(const char *who, uint16_t rpm);
 
-    void startBlinker();
-    void stopBlinker();
+    void startBlinker(const char *who);
+    void stopBlinker(const char *who);
 
-    void playHorn();
+    void playHorn(const char *who);
 
     void loop();
+
+    void stopAll();
 
 private:
     int _bclkPin, _lrclkPin, _dinPin;
@@ -34,17 +50,20 @@ private:
     volatile bool _hornOn;
 
     volatile uint16_t _engineRpm;
+    AudioClip *musicItem;
+    std::list<AudioClip *> _clipList;
+    AudioClip *engineStartItem;
+    AudioClip *engineRuningItem;
+    //AudioClip *engineStopItem;
+    AudioClip *blinkerRunItem;
+    //AudioClip *blinkerEndItem; 
+    //AudioClip *blinkerStartItem;
+    AudioClip *hornItem;
 
-    SampleItem *engineStartItem;
-    SampleItem *hornItem;
-    SampleItem *blinkerItem;
-    SampleItem *engineRuningItem;
-
-    AudioOutputI2S *outI2S;
-    AudioOutputMixer *mixer;
 
     static void _soundControllerTask(void *param);
     static void _loopTask(void *param);
+    void serialPrint(const char *procName, const char *who);
 };
 
 #endif // SOUND_CONTROLLER_H
