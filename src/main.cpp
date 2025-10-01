3add6e562c27f366b0ba2e83b453b5b2898e149b
Pawel A. <pawel.adamski25@wp.pl>
2025-10-01 20:57:58 +0200

    move definitions to header file

--- src/main.cpp
+++ src/main.cpp
@@ -6,40 +6,8 @@
 #include "PowerManager.h"
 #include "LightLedController.h"
 #include "AudioClipController.h"
+#include "definitions.h"
 
-/* MOTOR DRIVER */
-#define PWMA 32
-#define AIN2 33
-#define AIN1 25
-#define STBY 26
-#define BIN1 27
-#define BIN2 14
-#define PWMB 12
-
-/* LEDS */
-#define LED_STATUS 21
-
-/* LIGHTS */
-#define LED_LEFT_INDICATOR 18
-#define LED_BRAKE 5
-#define LED_MAIN_REAR 17
-#define LED_REVERSE 16
-#define LED_AUX 4
-#define LED_RIGHT_INDICATOR 0
-
-/* SENSORS  */
-#define CHARGER_DETECT_PIN 34
-
-/* SOUND */
-#define I2S_DIN 23
-#define I2S_BCLK 2
-#define I2S_LRCLK 19
-
-/* SD CARD */
-#define SD_MISO 35
-#define SD_SCK 13
-#define SD_MOSI 22
-#define SD_CS 15
 bool IsShown = false;
 
 PowerManager powerManager(CHARGER_DETECT_PIN, LED_STATUS);
